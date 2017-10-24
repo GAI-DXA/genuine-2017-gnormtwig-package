58a8d1a397973771c7b025d598fbd4f8cb84071e
Mike Dawson <randomletters@users.noreply.github.com>
2017-10-24 12:49:55 -0400

    Refactor and abstract a bit.

--- src/Gnormtwig.php
+++ src/Gnormtwig.php
@@ -22,8 +22,28 @@ class Gnormtwig {
   protected $sourcePath;
   // The pattern to search for source twig files.
   protected $sourcePattern;
+  // The twig environment.
+  protected $twigEnvironment;
+  // The twig loader.
+  protected $twigLoader;
 
-
+  /**
+   * Constructor.
+   *
+   * Available config keys:
+   *
+   *  baseDir:    The location of the central Gnorm folder.
+   *  source:     The location of the source directory (relative to the base).
+   *  pattern:    The pattern to use when searching for source twig files
+   *              (relative to the base).
+   *  dest:       The location to output the built html (relative to the base).
+   *  data:       The location of the JSON data directory (relative to the base).
+   *  namespaces: An array of namespace => locations to use as namespaces.
+   *  global:     The location of the global JSON File (relative to the base).
+   *  isBuild:    Indicates if output is a final build.
+   *
+   * @param $config
+   */
   public function __construct($config) {
 
     $this->baseDir = $config['baseDir'];
@@ -32,37 +52,28 @@ class Gnormtwig {
     $this->sourcePattern = $config['pattern'];
     $this->buildDir = $config['dest'];
     $this->buildPath = $this->baseDir . '/' . $this->buildDir;
-    $this->jsonPath = $config['data'];
+    $this->jsonPath = $this->baseDir . '/' . $config['data'];
     $this->namespaces = $config['namespaces'];
     $this->isBuild = $config['isBuild'];
 
     // Load the global json and set the build flag.
-    $this->globalJson = $this->getJson($config['global']);
-    $this->globalJson['isBuild'] = $config['isBuild'];
+    $this->globalJson = $this->getJson($this->baseDir . '/' . $config['global']);
+    $this->globalJson['isBuild'] = $this->isBuild;
 
   }
 
+  /**
+   * Render.
+   */
   public function render() {
     error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
 
     try {
-      // Load twig.
-      $loader = new \Twig_Loader_Filesystem($this->baseDir);
-
-      // Add the namespaces.
-      foreach ($this->namespaces as $key => $location) {
-        $loader->addPath($this->baseDir . '/' . $location, $key);
-      }
-
       // Setup the twig environment.
-      $twig = new \Twig_Environment($loader, array(
-        'cache' => FALSE,
-        'debug' => TRUE,
-        'autoescape' => FALSE,
-      ));
+      $twig = $this->getEnvironment();
 
       // Loop through each file that matches the source pattern.
-      foreach (glob($this->baseDir . '/' . $this->sourcePattern) as $filename) {
+      foreach ($this->getfiles() as $filename) {
         $this->renderFile($filename, $twig);
       }
     } catch (\Throwable $e) {
@@ -70,6 +81,59 @@ class Gnormtwig {
     }
   }
 
+  /**
+   * Get Loader.
+   *
+   * @return \Twig_Loader_Filesystem
+   * @throws \Twig_Error_Loader
+   */
+  public function getLoader(){
+    if (!$this->twigLoader) {
+      // Load twig.
+      $this->twigLoader = new \Twig_Loader_Filesystem($this->baseDir);
+
+      // Add the namespaces.
+      foreach ($this->namespaces as $key => $location) {
+        $this->twigLoader->addPath($this->baseDir . '/' . $location, $key);
+      }
+    }
+
+    return $this->twigLoader;
+  }
+
+  /**
+   * Get Environment.
+   *
+   * @return \Twig_Environment'
+   */
+  public function getEnvironment(){
+    if (!$this->twigEnvironment) {
+      // Setup the twig environment.
+      $this->twigEnvironment = new \Twig_Environment($this->getLoader(), array(
+        'cache' => FALSE,
+        'debug' => TRUE,
+        'autoescape' => FALSE,
+      ));
+    }
+
+    return $this->twigEnvironment;
+  }
+
+  /**
+   * Get Files.
+   *
+   * @return array
+   */
+  protected function getfiles(){
+    return glob($this->baseDir . '/' . $this->sourcePattern);
+  }
+
+  /**
+   * Render Files.
+   *
+   * @param string $filename
+   * @param \Twig_Environment $twig
+   */
   protected function renderFile($filename, \Twig_Environment $twig) {
     try {
       // Get the name of the file without extension.
@@ -89,12 +153,12 @@ class Gnormtwig {
       // Write the output file.
       $destination = $this->buildPath . "/{$basename}.html";
       file_put_contents($destination, $rendered);
+
     } catch (\Twig_Error $e) {
       $context = $e->getSourceContext();
       echo $e->getRawMessage() . "\n"
         . $context->getName() . " line: " . $e->getLine()
-        . "\n" . $context->getCode()
-        . "\n";
+        . "\n" . $context->getCode() . "\n";
     } catch (\Throwable $e) {
       echo $e->getMessage() . "\n";
       $this->outputTwigTrace($e->getTrace(), $twig);
@@ -119,6 +183,7 @@ class Gnormtwig {
       }
     }
     else {
+      echo "No JSON file: $file_path\n";
       return [];
     }
   }
